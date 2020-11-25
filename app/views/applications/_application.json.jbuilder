b847bd2b22c64041d7fa1e659cdb895b938a666e
Phelim Dunleavy <dunleavy.phelim@gmail.com>
2020-11-25 11:07:04 +0000

    skeleton drag and drop app

---
+++ app/views/applications/_application.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! application, :id, :user_id, :status, :company_name, :created_at, :updated_at
+json.url application_url(application, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_213525) do
+ActiveRecord::Schema.define(version: 2020_11_24_215317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "status"
+    t.string "company_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_applications_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_11_24_213525) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "applications", "users"
 end
